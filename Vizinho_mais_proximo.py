e17486caaffff0eec0338ccd6b66939e77e82056
FuscaRetratil <84513910+FuscaRetratil@users.noreply.github.com>
2024-09-03 22:59:33 -0300

    Update Vizinho_mais_proximo.py

--- Vizinho_mais_proximo.py
+++ Vizinho_mais_proximo.py
@@ -1,14 +1,15 @@
 import random
 import math
-import time
 
 
 def ler_arquivo(nome_arquivo):
-    grafo = []
     with open(nome_arquivo, 'r') as arquivo:
-        for linha in arquivo:
-            valores = linha.split()
-            grafo += [[int(valores[0]), float(valores[1]), float(valores[2])]]
+        linhas = arquivo.readlines()
+    num_vertices = len(linhas)
+    grafo = [[0] * 3 for _ in range(num_vertices)]
+    for i, linha in enumerate(linhas):
+        valores = linha.split()
+        grafo[i] = [int(valores[0]), float(valores[1]), float(valores[2])]
     return grafo
 
 
@@ -16,17 +17,12 @@ def calcular_distancia(x1, y1, x2, y2):
     return math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
 
 
-def vmp(grafo):
-    inicio = time.time()
+def vizinho_mais_proximo(grafo):
     print("Começando . . .")
     vertice_atual = random.choice(range(len(grafo)))
     caminho_final = [vertice_atual]
-    vertices_restantes = []
+    vertices_restantes = [i for i in range(len(grafo)) if i != vertice_atual]
     custo = 0
-
-    for i in range(len(grafo)):
-        vertices_restantes.append(i)
-    vertices_restantes.remove(vertice_atual)
 
     while len(vertices_restantes) != 0:
         distancia_minima = float('inf')
@@ -38,18 +34,25 @@ def vmp(grafo):
                 proximo_vertice = i
         custo += distancia_minima
 
-        caminho_final.append(proximo_vertice)
-        vertices_restantes.remove(proximo_vertice)
-        vertice_atual = proximo_vertice
+        caminho_final = caminho_final + [proximo_vertice]  #adiciona ao caminho final
+        vertices_restantes = [v for v in vertices_restantes if v != proximo_vertice]  #remove da lista de restantes
+        vertice_atual = proximo_vertice  #atualiza o vertice atual
 
-    custo += calcular_distancia(vertice_atual, 0, caminho_final[0], 0)
-    caminho_final.append(caminho_final[0])
+    custo += calcular_distancia(grafo[vertice_atual][1], grafo[vertice_atual][2], grafo[caminho_final[0]][1],grafo[caminho_final[0]][2])
+    caminho_final = caminho_final + [caminho_final][0]
 
-    print("Caminho final:", caminho_final)
-    print("Custo total:", custo)
-    print("Tempo de execução:", time.time() - inicio)
+    return custo
 
 
+custos = ""
 nome_arquivo = "coordenadas.txt"
-Grafo = ler_arquivo(nome_arquivo)
-vmp(Grafo)
+for i in range(30):
+    Grafo = ler_arquivo(nome_arquivo)
+    custo = vizinho_mais_proximo(Grafo)
+    custos += str(custo) + ","
+    print(f"Execução {i + 1}: Custo = {custo}")
+
+custos_lista = [float(custo) for custo in custos.strip(',').split(',')]
+
+media_custos = sum(custos_lista) / len(custos_lista)
+print(f"Média dos custos após 30 execuções: {media_custos}")
