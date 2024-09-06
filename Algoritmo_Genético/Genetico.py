6c3024bc18831e10fc35b078a4aaad14074f752c
IgorBLQ <igor.queiroz@ufrpe.br>
2024-09-05 23:10:08 -0300

    Update Genetico.py

--- Algoritmo_Genético/Genetico.py
+++ Algoritmo_Genético/Genetico.py
@@ -1,16 +1,17 @@
-from random import random, randint
+
+from random import random, randint, seed
 from typing import List, Tuple, Callable
 from math import floor
 import math
 
 berlin52 = [None]*52
 ponto = {}
-
 with open('berlin52.tsp','r') as arq:
     linhas = arq.readlines()[6:-2]
     for i in range(len(linhas)):
         berlin52[i] = linhas[i].split()
         ponto[int(berlin52[i][0])] = (float(berlin52[i][1]), float(berlin52[i][2]))
+        berlin52[i] = [int(berlin52[i][0]), float(berlin52[i][1]), float(berlin52[i][2])]
         
             
 
@@ -18,32 +19,65 @@ with open('berlin52.tsp','r') as arq:
 
 
 def individuo (pontos, tam_indiv = 51):
+    tam_pontos = len(pontos)
     individuo = [None] * tam_indiv
     i=0
     while i < tam_indiv:
-        num = randint(2, len(pontos))
+        num = randint(2, tam_pontos)
         if num not in individuo:
             individuo[i] = num
             i+=1
     return individuo
 
-def populacao_inicial (pontos, tamanho):
+def populacao_inicial (pontos, tamanho, tam_gulosos, grafo):
     populacao = [None] * tamanho
-    for i in range(tamanho):
+    for i in range(tam_gulosos):
+        inicio_guloso = randint(1,51)
+        populacao[i] = guloso(grafo, inicio_guloso)
+    for i in range(tam_gulosos, tamanho):
         populacao[i] = individuo(pontos)
     return populacao
     
+def guloso (grafo, inicio):
+    vertice_atual = inicio
+    caminho_final = [vertice_atual]
+    vertices_restantes = [i for i in range(len(grafo)) if i != vertice_atual and i!=0]
+    custo = 0
+
+    while len(vertices_restantes) != 0:
+        distancia_minima = float('inf')
+        proximo_vertice = None
+        for i in vertices_restantes:
+            distancia = distEuc(grafo[vertice_atual][1], grafo[vertice_atual][2], grafo[i][1], grafo[i][2])
+            if distancia < distancia_minima:
+                distancia_minima = distancia
+                proximo_vertice = i
+        custo += distancia_minima
+
+        caminho_final = caminho_final + [proximo_vertice]  #adiciona ao caminho final
+        vertices_restantes = [v for v in vertices_restantes if v != proximo_vertice]  #remove da lista de restantes
+        vertice_atual = proximo_vertice  #atualiza o vertice atual
+
+    custo += distEuc(grafo[vertice_atual][1], grafo[vertice_atual][2], grafo[caminho_final[0]][1],grafo[caminho_final[0]][2])
+    caminho_final = caminho_final
+
+    for i, ponto in enumerate (caminho_final):
+        ponto += 1
+        caminho_final[i] = ponto
+
+    return caminho_final
 
 def distEuc(x1,y1,x2,y2):
     distancia = math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
     return distancia
 
 def aptidao_individuo(individuo):
+    tam_individuo = len(individuo)
     distancia = 0
     x, y = ponto[individuo[0]]
     distancia += distEuc(565, 575, x, y)
     pos = 0       
-    for pos in range(1, len(individuo)-1):
+    for pos in range(1, tam_individuo - 1):
             x1, y1 = ponto[individuo[pos]]
             x2, y2 = ponto[individuo[pos+1]]
             distancia += distEuc(x1,y1,x2,y2)
@@ -56,9 +90,10 @@ def aptidao_individuo(individuo):
     return 1/distancia, distancia
 
 def aptidao_populacao(populacao):
-    aptidao = [None]*len(populacao)
-    custo = [None]*len(populacao)
-    for i in range (len(populacao)):
+    tam_populacao = len(populacao)
+    aptidao = [None]*tam_populacao
+    custo = [None]*tam_populacao
+    for i in range (tam_populacao):
         aptidao[i], custo[i] = aptidao_individuo(populacao[i])
     return aptidao, custo
 
@@ -74,11 +109,11 @@ def preenche_filho(filho, tamanho, pai, ponto_cruzamento):
     return filho
                 
     
-def cruzamento_pais (pai1, pai2, taxa_cruzamento):
+def cruzamento_pais (pai1, pai2, taxa_cruzamento):   #crossover com OX
     if random() <= taxa_cruzamento:
-        tamanho = len(pai1)
-        ponto_cruzamento = randint(1, len(pai1) - 1)
-        ponto_cruzamento2 = randint(ponto_cruzamento, len(pai1)-1)
+        tam_pai = len(pai1)
+        ponto_cruzamento = randint(1, tam_pai - 1)
+        ponto_cruzamento2 = randint(ponto_cruzamento, tam_pai - 1)
         tam_filho = len(pai1)
 
         filho_1 = [None] * tam_filho
@@ -87,20 +122,18 @@ def cruzamento_pais (pai1, pai2, taxa_cruzamento):
         filho_1[ponto_cruzamento:ponto_cruzamento2 + 1] = pai1[ponto_cruzamento:ponto_cruzamento2+1]
         filho_2[ponto_cruzamento:ponto_cruzamento2 + 1] = pai2[ponto_cruzamento:ponto_cruzamento2+1]
 
-        filho_1 = preenche_filho(filho_1, tamanho, pai2, ponto_cruzamento2 + 1)
-        filho_2 = preenche_filho(filho_2, tamanho, pai1, ponto_cruzamento2 + 1)
-
-        filho_1 = mutacao(filho_1, 0.1)
-        filho_2 = mutacao(filho_2, 0.1)
+        filho_1 = preenche_filho(filho_1, tam_pai, pai2, ponto_cruzamento2 + 1)
+        filho_2 = preenche_filho(filho_2, tam_pai, pai1, ponto_cruzamento2 + 1)
 
         return filho_1, filho_2
     return pai1, pai2
 
 
 def cruzamento(pais, taxa_cruzamento):
-    lista_filhos = [None] * len(pais)
+    tam_pais = len(pais)
+    lista_filhos = [None] * tam_pais
     i = 0
-    while i < len(pais):
+    while i < tam_pais:
         filho1, filho2 = cruzamento_pais(pais[i], pais[i+1], taxa_cruzamento)
         lista_filhos[i] = filho1
         lista_filhos[i+1] = filho2
@@ -119,14 +152,15 @@ def troca(lista, id1, id2):
     lista[id1], lista[id2] = lista[id2], lista[id1]
     
 def torneio(populacao, lista_apt):
-    p1 = randint(0, len(populacao) -1)
-    p2 = randint(0, len(populacao) -1)
+    tam_populacao = len(populacao)
+    p1 = randint(0, tam_populacao -1)
+    p2 = randint(0, tam_populacao -1)
     if p1 != p2:
         if aptidao_individuo(populacao[p1]) > aptidao_individuo(populacao[p2]):
             return p1
     return p2
     
-def roleta(populacao, lista_apt):
+def roleta(populacao, lista_apt):   # <---- nem ajeitei essa funcao roleta, pode jogar fora
     soma_atual = 0
     soma_roleta = sum(lista_apt)
     n_sorteado = random() * soma_roleta
@@ -136,63 +170,69 @@ def roleta(populacao, lista_apt):
             return i
     
 def selecao_pais(populacao, lista_apt, funcao):
-    lista_pais = [None] * len(populacao)
-    for i in range(len(populacao)):
+    tam_populacao = len(populacao)
+    lista_pais = [None] * tam_populacao
+    for i in range(tam_populacao):
         id_sel = funcao(populacao, lista_apt)
         lista_pais[i] = populacao[id_sel]
     return lista_pais
 
 
 
-def selecao_sobreviventes(populacao, aptidoes, filhos, aptidoes_filhos, tam_elites):
+def selecao_sobreviventes(populacao, aptidoes, filhos, aptidoes_filhos, tam_elite):
     tam_populacao = len(populacao)
-    tam_restante = tam_populacao - tam_elites
+    tam_individuos_restantes = tam_populacao - tam_elite
 
     pop_aptidao = ordena_populacao(populacao, aptidoes)
-    elite = [individuo for individuo, i in pop_aptidao[:tam_elites]]
+    elite = [individuo for individuo, i in pop_aptidao[:tam_elite]]
     
     filhos_aptidao = ordena_populacao(filhos, aptidoes_filhos)
-    novos_individuos = [individuo for individuo, i in filhos_aptidao[:tam_restante]]
+    novos_individuos = [individuo for individuo, i in filhos_aptidao[:tam_individuos_restantes]]
     
     nova_populacao = elite + novos_individuos 
-    nova_aptidao = aptidoes[:tam_elites] + [aptidao for i, aptidao in filhos_aptidao[:tam_restante]]
+    nova_aptidao = aptidoes[:tam_elite] + [aptidao for i, aptidao in filhos_aptidao[:tam_individuos_restantes]]
     
     return nova_populacao, nova_aptidao
 
+def heapify(arr, n, i, pos_apt):
+    maior = i 
+    esq = 2 * i + 1  
+    dir = 2 * i + 2  
+    
+    if esq < n and arr[esq][pos_apt] < arr[maior][pos_apt]:
+        maior = esq
 
-def heapify(arr, n, i):
-
-    maior = i  
-    esquerda = 2 * i + 1  
-    direita = 2 * i + 2  
-
-    if esquerda < n and arr[esquerda] > arr[maior]:
-        maior = esquerda
-
-    if direita < n and arr[direita] > arr[maior]:
-        maior = direita
+    if dir < n and arr[dir][pos_apt] < arr[maior][pos_apt]:
+        maior = dir
 
     if maior != i:
-        arr[i], arr[maior] = arr[maior], arr[i]
-        heapify(arr, n, maior)
+        arr[maior], arr[i] = arr[i], arr[maior]  
+        
+        heapify(arr, n, maior, pos_apt)
 
-def heapsort(arr):
-
-    n = len(arr)
+def heap_sort(pop_apt, pos_apt):
+    n = len(pop_apt)
 
     for i in range(n // 2 - 1, -1, -1):
-        heapify(arr, n, i)
+        heapify(pop_apt, n, i, pos_apt)
 
     for i in range(n - 1, 0, -1):
-        arr[0], arr[i] = arr[i], arr[0]
-
-        heapify(arr, i, 0)
+        pop_apt[0], pop_apt[i] = pop_apt[i], pop_apt[0]
+        heapify(pop_apt, i, 0, pos_apt)
 
 
 def ordena_populacao(populacao, lista_apt):
     populacao = list(zip(populacao, lista_apt))
-    populacao.sort(key=lambda x: x[1], reverse=True)
+    #populacao.sort(key=lambda x: x[1], reverse=True)
+    heap_sort(populacao,1)
     return populacao
+
+
+def elitismo (populacao, populacao_geracao, tam_elite):
+    elite = populacao[:tam_elite]
+    populacao_geracao = populacao_geracao
+    populacao_geracao[:tam_elite] = elite
+    return populacao_geracao
 
 '''
 def imprimir_populacao(pop, apt, geracao):
@@ -204,10 +244,10 @@ def imprimir_populacao(pop, apt, geracao):
     print("*****************************")
 '''
 
-def evolucao(pontos, tamanho, taxa_cruzamento, taxa_mutacao, n_geracoes, funcao, tam_elites):
-    total = [float('inf')]*10000
+def evolucao(pontos, tamanho, qtd_guloso, grafo, taxa_cruzamento, taxa_mutacao, n_geracoes, funcao, tam_elite):
+    total = [float('inf')]*n_geracoes
     maior_apt = float('-inf')
-    pop = populacao_inicial(ponto.keys(), tamanho)
+    pop = populacao_inicial(ponto.keys(), tamanho, qtd_guloso, grafo)
     lista_apt, custo = aptidao_populacao(pop)
     print(min(custo))
     
@@ -215,6 +255,8 @@ def evolucao(pontos, tamanho, taxa_cruzamento, taxa_mutacao, n_geracoes, funcao,
         print('geracao->', geracao)
         pais = selecao_pais(pop, lista_apt, funcao)
         filhos = cruzamento(pais, taxa_cruzamento)
+        filhos = mutacao(filhos, taxa_mutacao)
+        filhos = elitismo(pop, filhos, tam_elite)
         apt_filhos, custos = aptidao_populacao(filhos)
         total[geracao] = min(custos)
         aptidao_geracao = max(apt_filhos)
@@ -226,8 +268,7 @@ def evolucao(pontos, tamanho, taxa_cruzamento, taxa_mutacao, n_geracoes, funcao,
         print('custo-geracao->', min(custos))
         print('menor-custo->', min(total))
         print('****************************************************')
-        pop, lista_apt = selecao_sobreviventes(pop, lista_apt, filhos, apt_filhos, tam_elites)
-        
+        pop, lista_apt = selecao_sobreviventes(pop, lista_apt, filhos, apt_filhos, tam_elite)
     print('CUSTO MÍNIMO ENCONTRADO EM %d GERACOES ->' %(n_geracoes), min(total))
     print('CAMINHO DE MENOR CUSTO ->', menor_caminho)
     return pop, lista_apt
@@ -235,15 +276,16 @@ def evolucao(pontos, tamanho, taxa_cruzamento, taxa_mutacao, n_geracoes, funcao,
 
 
 def principal():
-    pontos = ponto.keys()
+    qtd_gulosos = 30     # <--- qtd da populacao que vai ser definida por guloso
+    grafo = berlin52     #<---- lista de listas para ser o grafo da funcao gulosa
+    pontos = ponto.keys()  #<---- ponto é o dicionario com chaves de 1 a 52 e suas coordenadas em tuplas
     taxa_cruzamento = 0.9
-    taxa_mutacao = 0.1
-    tam_individuo = 51
-    tam_populacao = 60
-    tam_elites = 30
-    n_geracoes = 800
-    funcao = torneio
-    pop, lista_apt = evolucao(pontos, tam_populacao, taxa_cruzamento, taxa_mutacao, n_geracoes, funcao, tam_elites)
+    taxa_mutacao = 0.05
+    tam_populacao = 100
+    tam_elites = 10  
+    n_geracoes = 100
+    funcao = torneio     #<---- a funcao usada é torneio
+    pop, lista_apt = evolucao(pontos, tam_populacao, qtd_gulosos, grafo, taxa_cruzamento, taxa_mutacao, n_geracoes, funcao, tam_elites)
     
     
     
